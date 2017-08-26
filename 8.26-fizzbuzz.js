e12e7f07a046303e464e93c892dc4270da48a2dd
VoidWatcher <book3h@gmail.com>
2017-08-26 14:04:20 +0800

    fix && problem

--- 8.26-fizzbuzz.js
+++ 8.26-fizzbuzz.js
@@ -1,6 +1,6 @@
 function fizzbuzz (n) {
     for (let i = 1; i <= n; i++) {
-        if (i % 3 == 0 & i % 5 == 0) {
+        if (i % 3 == 0 && i % 5 == 0) {
             console.log('fizz buzz');
         }
         else if (i % 3 == 0 & i % 5 != 0) {
