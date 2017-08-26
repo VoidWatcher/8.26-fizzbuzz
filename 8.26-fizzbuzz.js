c40422bbc4031addf2c28dd1fa5102dc110a625c
VoidWatcher <book3h@gmail.com>
2017-08-26 14:01:33 +0800

    first commit

---
+++ 8.26-fizzbuzz.js
@@ -0,0 +1,17 @@
+function fizzbuzz (n) {
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 == 0 & i % 5 == 0) {
+            console.log('fizz buzz');
+        }
+        else if (i % 3 == 0 & i % 5 != 0) {
+            console.log('fizz');
+        }
+        else if (i % 3 != 0 & i % 5 == 0) {
+            console.log('buzz');
+        }
+        else {
+            console.log(i);
+        }
+    }
+}
+fizzbuzz(15);
